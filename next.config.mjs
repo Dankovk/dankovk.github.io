e3ccdcb5058d9f0d39e0c57ff5b8e3fb0bd01d2a
Даньков Олександр <dankov.kiev@gmail.com>
2024-03-24 22:17:32 +0200

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "aceternity.com",
+        port: "",
+        pathname: "images/products/thumbnails/new",
       },
     ],
   },
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { HeroParallaxDemo } from "@/components/wrappers/hero";
 
 export default function Home() {
